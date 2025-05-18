8e8b7d3b2ba1857744c69b74530adbf44c72ca52
Mitch <mitchenfield@gmail.com>
2025-05-18 16:53:46 -0400

    Added header

---
+++ src/Header/container.tsx
@@ -0,0 +1,110 @@
+import { Box, Grid, Typography, useTheme } from "@mui/material";
+import { FC, useState } from "react";
+
+const HeaderContainer: FC = ({}) => {
+  // Component variables
+  const theme = useTheme();
+
+  // Component state
+
+  // Component effects
+
+  // Function binding
+
+  return (
+    <>
+      <Box
+        sx={{
+          position: "relative",
+        }}
+      >
+        <Box
+          sx={{
+            background: (theme) =>
+              `linear-gradient(to right, ${theme.palette.primary.main}, ${theme.palette.secondary.main})`,
+            zIndex: 3,
+            position: "relative",
+          }}
+        >
+          <Grid
+            container
+            direction="row"
+            alignItems="center"
+            p={theme.spacing(0, 8)}
+            height="90px"
+          >
+            <Grid size="grow">
+              <Typography component="h1" fontSize="36px" color="white.main">
+                Mitchell Enfield
+              </Typography>
+            </Grid>
+            <Grid container direction="row" gap={1}>
+              <Grid>
+                <Typography component="h2" fontSize="24px" color="white.main">
+                  (561) 699-0925
+                </Typography>
+              </Grid>
+              <Grid>
+                <Typography component="h2" fontSize="24px" color="white.main">
+                  |
+                </Typography>
+              </Grid>
+              <Grid>
+                <Typography component="h2" fontSize="24px" color="white.main">
+                  MitchEnfield@gmail.com
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Box>
+
+        <Box
+          sx={{
+            position: "absolute",
+            top: 50,
+            left: 0,
+            right: 0,
+            zIndex: 2,
+          }}
+        >
+          <svg viewBox="0 0 500 500" preserveAspectRatio="xMinYMin meet">
+            <defs>
+              <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="0%">
+                <stop
+                  offset="0%"
+                  style={{ stopColor: theme.palette.primary.main }}
+                />
+                <stop
+                  offset="100%"
+                  style={{ stopColor: theme.palette.secondary.main }}
+                />
+              </linearGradient>
+            </defs>
+            <path
+              d="M0.00,20 C149.99,80.00 271.49,-20 500.00,20 L500.00,0.00 L0.00,0.00 Z"
+              fill="url(#grad)"
+            />
+          </svg>
+        </Box>
+        <Box
+          sx={{
+            position: "absolute",
+            top: 75,
+            left: 0,
+            right: 0,
+            zIndex: 1,
+          }}
+        >
+          <svg viewBox="0 0 500 500" preserveAspectRatio="xMinYMin meet">
+            <path
+              d="M0.00,20 C149.99,80.00 271.49,-20 500.00,20 L500.00,0.00 L0.00,0.00 Z"
+              fill={theme.palette.accent.main}
+            />
+          </svg>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default HeaderContainer;
---
+++ src/theme.ts
@@ -0,0 +1,157 @@
+import {
+  PaletteColor,
+  PaletteColorOptions,
+  createTheme,
+} from "@mui/material/styles";
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    tertiary: PaletteColor;
+    white: PaletteColor;
+    accent: PaletteColor;
+  }
+
+  interface PaletteOptions {
+    tertiary: PaletteColorOptions;
+    white: PaletteColorOptions;
+    accent: PaletteColorOptions;
+  }
+
+  interface TypographyVariants {
+    standardInverted: React.CSSProperties;
+  }
+
+  interface TypographyVariantsOptions {
+    standardInverted?: React.CSSProperties;
+  }
+}
+
+// Update the Typography's variant prop options
+declare module "@mui/material/Typography" {
+  interface TypographyPropsVariantOverrides {
+    standardInverted: true;
+  }
+}
+
+declare module "@mui/material/Button" {
+  interface ButtonPropsColorOverrides {
+    tertiary: true;
+    white: true;
+    accent: true;
+  }
+}
+
+const themeStyles = {
+  fontFamily: "roboto, arial, san_serif",
+  fontRegular: "roboto, arial, san_serif",
+  fontMedium: "roboto, arial, san_serif",
+  fontBold: "roboto, arial, san_serif",
+  fontLight: "roboto, arial, san_serif",
+  fontAccent: "'allstate_accent', arial, san_serif",
+  borderRadius: 0,
+  textH1Size: "2.5rem",
+  textH2Size: "1.5rem",
+  textH3Size: "1.375rem",
+  textH4Size: "1rem",
+  textH5Size: "0.875rem",
+  textH6Size: "1rem",
+  textInputLabel: "0.875rem",
+  tertiary: "#DADADA",
+  white: "#FFF",
+  text: "#323232",
+  primary: "#396E72",
+  background: "#FAFAFA",
+  secondary: "#6CD0D8",
+  accent: "#2BA6AF",
+};
+
+const { palette } = createTheme();
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: themeStyles.primary,
+    },
+    secondary: {
+      main: themeStyles.secondary,
+    },
+    text: {
+      primary: themeStyles.text,
+    },
+    background: {
+      default: themeStyles.background,
+    },
+    tertiary: palette.augmentColor({
+      color: { main: themeStyles.tertiary },
+    }),
+    white: palette.augmentColor({
+      color: { main: themeStyles.white },
+    }),
+    accent: palette.augmentColor({
+      color: { main: themeStyles.accent },
+    }),
+  },
+
+  typography: {
+    fontFamily: themeStyles.fontFamily,
+    // h1: {
+    //   fontFamily: themeStyles.fontBold,
+    //   fontSize: themeStyles.textH1Size,
+    // },
+    // h2: {
+    //   fontFamily: themeStyles.fontMedium,
+    //   fontSize: themeStyles.textH2Size,
+    // },
+    // h3: {
+    //   fontFamily: themeStyles.fontBold,
+    //   fontSize: themeStyles.textH3Size,
+    // },
+    // h4: {
+    //   fontFamily: themeStyles.fontRegular,
+    //   fontSize: themeStyles.textH4Size,
+    // },
+    // h5: {
+    //   fontFamily: themeStyles.fontMedium,
+    //   fontSize: themeStyles.textH5Size,
+    // },
+    // h6: {
+    //   fontFamily: themeStyles.fontBold,
+    //   fontSize: themeStyles.textH6Size,
+    // },
+    // body1: {
+    //   color: themeStyles.textColor,
+    // },
+    // body2: {
+    //   color: themeStyles.textColor,
+    // },
+    // button: {
+    //   textTransform: "none",
+    //   fontFamily: themeStyles.fontBold,
+    // },
+  },
+
+  // shape: {
+  //   borderRadius: themeStyles.borderRadius,
+  // },
+
+  // components: {
+  //   MuiPaper: {
+  //     styleOverrides: {
+  //       root: {
+  //         borderRadius: "3px",
+  //       },
+  //     },
+  //   },
+  // },
+
+  // breakpoints: {
+  //   values: {
+  //     xs: 0,
+  //     sm: 600,
+  //     md: 1023, //This is where we switch to mobile
+  //     lg: 1200,
+  //     xl: 1536,
+  //   },
+  // },
+});
+
+export default theme;
